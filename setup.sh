9882d044a5d47bfe553eb33875b78eff52660d30
Da-Emp66 <asherbean10@gmail.com>
2024-02-08 01:12:31 -0500

    reverse waterworld working?

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 poetry install
-pip install "ray[rllib]"
+pip3 install "ray[rllib]"
 pip3 install torch==2.0.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117 --no-cache-dir
+pip3 install stable-baselines3 # Poetry strangely takes forever with this one, so we install it here...
